6cda7c29590bfe0fa80f802d8c9c279f74fe17c3
Fabian Hueber <fabi_boy@hotmail.de>
2022-12-14 09:40:04 +0100

    fixed Token verification

--- app/auth_handler.py
+++ app/auth_handler.py
@@ -1,7 +1,7 @@
 import time
 import jwt
 from jwt import DecodeError
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 from config import JWT_SECRET, JWT_ALGORITHM
 
 class TokenData(BaseModel):
@@ -9,25 +9,33 @@ class TokenData(BaseModel):
     expires : int
     company_id : int
     role : str
-    token_type: str
+
+    def is_valid(self):
+        # allow tokens without expire date for testing
+        return self.expires is not None and (self.expires == -1 or self.expires >= time.time())
 
     @staticmethod
     def from_token(token : str):
-        decoded_token = TokenData._decode_jwt(token)
 
+        decoded_token = TokenData._decode_jwt(token)
         if decoded_token is None:
             return None
 
-        return TokenData.parse_raw(decoded_token)
-
+        try:
+            parsed_token = TokenData.parse_obj(decoded_token)
+            if not parsed_token.is_valid():
+                return None
+            return parsed_token
+        except ValidationError:
+            return None
 
     @staticmethod
     def _decode_jwt(token: str):
         try:
             decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-            return decoded_token if decoded_token["expires"] >= time.time() else None
+            return decoded_token
         except DecodeError:
             return None
 
     def to_token(self):
-        return jwt.encode(self.json(), JWT_SECRET, algorithm=JWT_ALGORITHM)
+        return jwt.encode(self.dict(), JWT_SECRET, algorithm=JWT_ALGORITHM)
