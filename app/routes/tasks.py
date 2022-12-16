c78d6658f960847babb49a01e2abc89960bf5152
Fabian Hueber <fabi_boy@hotmail.de>
2022-12-16 14:16:14 +0100

    fixed bugs in get-tasks

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -1,14 +1,11 @@
 from typing import List, Union
 
-from fastapi import APIRouter, HTTPException, Header
+from fastapi import APIRouter, Header
 from config import dbm
 from backend_db_lib.models import User, LPAAudit
-
-from auth_handler import TokenData
 from dao.tasks import GetTasksDAO, TaskDAO
-from pydantic import BaseModel
 
-from app.helpers import validate_authorization
+from helpers.auth import validate_authorization
 
 router = APIRouter(
     prefix="/api/tasks",
@@ -20,7 +17,7 @@ router = APIRouter(
 @router.post("/get-tasks/")
 def get_tasks(authorization: Union[str, None] = Header(default=None)) -> GetTasksDAO:
     payload = validate_authorization(authorization)
-    user_id = payload['user_id']
+    user_id = int(payload['user_id'])
 
     with dbm.create_session() as session:
         user = session.query(User).get(user_id)
@@ -39,26 +36,31 @@ def get_tasks(authorization: Union[str, None] = Header(default=None)) -> GetTask
 def get_tasks_lpa(session, user) -> List[TaskDAO]:
     layer_id = user.layer_id
     group_id = user.group_id
-
     if (layer_id is not None) and (group_id is not None):
         audits = session.query(LPAAudit).filter(
-            (LPAAudit.assigned_group_id == group_id) |
-            (LPAAudit.assigned_layer_id == layer_id) & (LPAAudit.complete_datetime is None)
+            (
+                    (LPAAudit.assigned_group_id == group_id) |
+                    (LPAAudit.assigned_layer_id == layer_id) |
+                    (LPAAudit.audited_user_id == user.id)
+            ) &
+            (LPAAudit.complete_datetime.is_(None))
         )
 
         tasks = []
         for audit in audits:
-            assignee = ('Gruppe ' + audit.group.name) if audit.group is not None else ('Ebene ' + audit.layer.name)
+            assignee = f"Gruppe '{audit.assigned_group.group_name}'" if audit.assigned_group is not None \
+                else f"Ebene '{audit.assigned_layer.layer_name}'"
             recurrence = ('Spontaner' if audit.recurrent_audit else 'Geplanter')
 
-            title = f'{assignee} Audit in {recurrence}'
+            title = f"{recurrence} Audit für {assignee}"
 
             task = TaskDAO(
                 icon='lpa',
                 app_name='lpa',
                 title=title,
                 action='audit',
-                parameter=f'{audit.id}'
+                parameter=f'{audit.id}',
+                date=str(audit.due_date),
             )
             tasks.append(task)
         return tasks
