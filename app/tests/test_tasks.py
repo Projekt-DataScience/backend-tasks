da1a1d4ff79ef44ed3198349c47665c6f3a35fbc
Fahri Korkmaz <fahri.korkmaz@protonmail.ch>
2022-12-21 20:33:36 +0100

    Add TODO

--- app/tests/test_tasks.py
+++ app/tests/test_tasks.py
@@ -1,20 +1,11 @@
-# import time
-
 # from main import app
-# from auth_handler import TokenData
 # from fastapi.testclient import TestClient
 
 # test_client = TestClient(app)
 
-# def test_check_auth():
-#     # check authentication
-
-#     token = TokenData(
-#         user_id=1,
-#         expires=-1,
-#         company_id=0,
-#         role="admin",
-#     )
-#     token_data = token.to_token()
+def test_check_auth():
 #     response = test_client.post("/api/tasks/get-tasks/", json={'authorization' : token_data})
 #     assert response.status_code == 200
+
+    # TODO: Issue #15
+    assert True
