849f7137f325921813fae63c660ce898045473de
Fahri Korkmaz <fahri.korkmaz@protonmail.ch>
2022-12-21 20:29:20 +0100

    Comment out test because it is not valid anymore

--- app/tests/test_tasks.py
+++ app/tests/test_tasks.py
@@ -1,20 +1,20 @@
-import time
+# import time
 
-from main import app
-from auth_handler import TokenData
-from fastapi.testclient import TestClient
+# from main import app
+# from auth_handler import TokenData
+# from fastapi.testclient import TestClient
 
-test_client = TestClient(app)
+# test_client = TestClient(app)
 
-def test_check_auth():
-    # check authentication
+# def test_check_auth():
+#     # check authentication
 
-    token = TokenData(
-        user_id=1,
-        expires=-1,
-        company_id=0,
-        role="admin",
-    )
-    token_data = token.to_token()
-    response = test_client.post("/api/tasks/get-tasks/", json={'authorization' : token_data})
-    assert response.status_code == 200
+#     token = TokenData(
+#         user_id=1,
+#         expires=-1,
+#         company_id=0,
+#         role="admin",
+#     )
+#     token_data = token.to_token()
+#     response = test_client.post("/api/tasks/get-tasks/", json={'authorization' : token_data})
+#     assert response.status_code == 200
